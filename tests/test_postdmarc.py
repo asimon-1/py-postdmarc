fbff0f65a19e934dca99e26d872e09f660f90c91
Andrew Simon <asimon@domeng.com>
2020-01-21 09:17:36 -0500

    Initial commit

---
+++ tests/test_postdmarc.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestResponse(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_naiive_test(self):
+        pass
